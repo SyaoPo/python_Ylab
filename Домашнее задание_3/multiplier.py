190570bcaea5ffeb81b0257b71d12ea52969d3ac
Polina SyaoPO <92932332+SyaoPo@users.noreply.github.com>
2022-07-11 14:05:15 +0300

    Update multiplier.py

--- Домашнее задание_3/multiplier.py
+++ Домашнее задание_3/multiplier.py
@@ -1,2 +1,17 @@
+def my_decorator(func):
+    my_dict = {}
+
+    def wrapper(*args, **kwargs):
+        key = int(*args)
+        if key in my_dict.keys():
+            return my_dict[key]
+        result = func(*args, **kwargs)
+        my_dict[key] = result
+        return result
+
+    return wrapper
+
+
+@my_decorator
 def multiplier(number: int):
     return number * 2
