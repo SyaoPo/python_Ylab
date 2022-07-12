02715af23d95cbf12ea64635b5f3d52694970c0f
Polina SyaoPO <92932332+SyaoPo@users.noreply.github.com>
2022-07-12 12:23:47 +0300

    Update main.py

--- Домашнее задание_3/not_solid_code/main.py
+++ Домашнее задание_3/not_solid_code/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from heroes import Superman, SuperHero
+from heroes import Superman, SuperHero, Chuck_Noris
 from places import Kostroma, Tokyo
 from media import TV
 
@@ -8,10 +8,10 @@ def save_the_place(hero: SuperHero, place: Union[Kostroma, Tokyo]):
     hero.attack()
     if hero.can_use_ultimate_attack:
         hero.ultimate()
-    hero.create_news(place)
+    TV(hero, place).create_news()
 
 
 if __name__ == '__main__':
     save_the_place(Superman(), Kostroma())
     print('-' * 20)
-    save_the_place(SuperHero('Chack Norris', False), Tokyo())
+    save_the_place(Chuck_Noris(), Tokyo())
