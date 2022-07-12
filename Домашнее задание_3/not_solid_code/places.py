7bc5e9d6ca0784b9d9a501fd1e41e2394bbdc4fc
Polina SyaoPO <92932332+SyaoPo@users.noreply.github.com>
2022-07-12 12:18:18 +0300

    Update places.py

--- Домашнее задание_3/not_solid_code/places.py
+++ Домашнее задание_3/not_solid_code/places.py
@@ -1,15 +1,26 @@
 from abc import ABC, abstractmethod
 
 
-class Kostroma:
+class Places(ABC):
+    @abstractmethod
+    def get_antagonist(self):
+        pass
+    
+class Kostroma(Places):
     city_name = 'Kostroma'
 
     def get_orcs(self):
         print('Orcs hid in the forest')
+    
+    def get_antagonist(self):
+        self.get_orcs()
 
 
-class Tokyo:
+class Tokyo(Places):
     name = 'Tokyo'
 
     def get_godzilla(self):
         print('Godzilla stands near a skyscraper')
+    
+    def get_antagonist(self):
+        self.get_godzilla()
