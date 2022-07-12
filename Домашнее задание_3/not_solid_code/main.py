acc3d144828186ddcdb80b3d4f18c9dbab2a63d8
Polina SyaoPO <92932332+SyaoPo@users.noreply.github.com>
2022-07-12 12:20:14 +0300

    Update main.py

--- Домашнее задание_3/not_solid_code/main.py
+++ Домашнее задание_3/not_solid_code/main.py
@@ -1,7 +1,7 @@
 from typing import Union
 from heroes import Superman, SuperHero
 from places import Kostroma, Tokyo
-
+from media import TV
 
 def save_the_place(hero: SuperHero, place: Union[Kostroma, Tokyo]):
     hero.find(place)
