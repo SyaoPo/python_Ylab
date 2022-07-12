2fe9cf648a9baa2d6c8c4cfcd2825ff5e84de2d3
Polina SyaoPO <92932332+SyaoPo@users.noreply.github.com>
2022-07-12 12:13:24 +0300

    Update media.py

--- Домашнее задание_3/not_solid_code/media.py
+++ Домашнее задание_3/not_solid_code/media.py
@@ -1,21 +1,18 @@
-
-
-from places import Place
 from abc import abstractmethod
 
 
-class Media:
+class Media(ABC):
+    def __init__(self, hero, place):
+        self.place = place
+        self.hero = hero
+
     @abstractmethod
     def create_news(self):
         pass
 
 
 class TV(Media):
-    name = 'TV'
-
     def create_news(self, name_hero: str, place: Place):
         print(f'{self.name}: {name_hero} saved the {place.place_name}!')
 
 
-class NewsPaper(TV):
-    name = 'Newspaper'
